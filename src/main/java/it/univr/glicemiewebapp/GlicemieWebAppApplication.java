ed896ad86562599e2560ae41f0fe81a98de3c1f2
fazionpietro <pietrofazion04@gmail.com>
2025-08-18 17:19:35 +0200

    Initial commit

---
+++ src/main/java/it/univr/glicemiewebapp/GlicemieWebAppApplication.java
@@ -0,0 +1,17 @@
+package it.univr.glicemiewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GlicemieWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlicemieWebAppApplication.class, args);
+    }
+
+}
+
+
+
