07a34e6ccbd15293b67bd04d154d0d91fe47919f
fazionpietro <pietrofazion04@gmail.com>
2025-09-04 18:39:45 +0200

    creata pagina di prova Login

--- src/main/resources/frontend/web-interface/src/main.tsx
+++ src/main/resources/frontend/web-interface/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Login from './app/LoginPage/Login'
+import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+   <StrictMode>
+    <MantineProvider>
+      <Login />
+    </MantineProvider>
+  </StrictMode>
 )
