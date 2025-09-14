19c10b5bb020feb01072943855ff9046d140ac12
fazionpietro <pietrofazion04@gmail.com>
2025-09-14 11:11:54 +0200

    cambiate impostazioni server

--- backend/src/main/java/it/univr/glicemiewebapp/dto/RilevazioneUtenteDTO.java
+++ backend/src/main/java/it/univr/glicemiewebapp/dto/RilevazioneUtenteDTO.java
@@ -1,47 +1,52 @@
-import java.time.LocalDateTime;
+package it.univr.glicemiewebapp.dto;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import it.univr.glicemiewebapp.entity.Rilevazione;
 
 public class RilevazioneUtenteDTO {
-    private String id;
-    private double valore;
-    private LocalDateTime timestamp;
-    private String livello;
+  private UUID id;
+  private double valore;
+  private Instant timestamp;
+  private String livello;
 
-    public RilevazioneUtenteDTO(Rilevazione r){
-        this.id=r.getId();
-        this.valore = r.getValore();
-        this.timestamp= r.getTimestamp();
-    }
+  public RilevazioneUtenteDTO(Rilevazione r) {
+    this.id = r.getId();
+    this.valore = r.getValore();
+    this.timestamp = r.getTimestamp();
+  }
 
-public String getId() {
-        return id;
-    }
+  public UUID getId() {
+    return id;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public void setId(UUID id) {
+    this.id = id;
+  }
 
-    public double getValore() {
-        return valore;
-    }
+  public double getValore() {
+    return valore;
+  }
 
-    public void setValore(double valore) {
-        this.valore = valore;
-    }
+  public void setValore(double valore) {
+    this.valore = valore;
+  }
 
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
+  public Instant getTimestamp() {
+    return timestamp;
+  }
 
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
+  public void setTimestamp(Instant timestamp) {
+    this.timestamp = timestamp;
+  }
 
-    public String getLivello() {
-        return livello;
-    }
+  public String getLivello() {
+    return livello;
+  }
 
-    public void setLivello(String livello) {
-        this.livello = livello;
-    }
+  public void setLivello(String livello) {
+    this.livello = livello;
+  }
 }
